969bf2786e91e95f4fce8d83f8c6e790596b4c27
emiliaplater <herman-dev@outlook.com>
2023-04-19 16:19:16 +0200

    MM#1: capture frame rebuild -v0.1

--- Modules/mainModule.py
+++ Modules/mainModule.py
@@ -2,8 +2,8 @@ import cv2
 
 
 
-capture = cv2.VideoCapture(0)
-keyFrame = 1
+capture = cv2.VideoCapture('../vids/#')
+timeFrame = 10
 
 while True:
     ret, frame = capture.read()
@@ -12,9 +12,10 @@ while True:
 
     cv2.imshow('Frame', frame)
 
-    key = cv2.waitKey(keyFrame) & 0xFF
+    key = cv2.waitKey(timeFrame) & 0xFF
     if key == ord('q'):
         break
 
 capture.release()
 cv2.destroyAllWindows()
+
