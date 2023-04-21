a94f5e40bc12406a6778d7cd0ed4fc96af68048b
emiliaplater <herman-dev@outlook.com>
2023-04-21 16:20:22 +0200

    BM: binary module added

---
+++ Modules/binaryModule.py
@@ -0,0 +1,13 @@
+import cv2
+#modules
+from algorithmModule import algorithmModule
+
+
+
+def binaryModule(frame):
+    gray_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_roi = cv2.GaussianBlur(gray_roi, (5, 5), 0)
+
+    _, threshold = cv2.threshold(gray_roi, 20, 255, cv2.THRESH_BINARY_INV)
+
+    return algorithmModule(threshold, frame)
