1443e65b1e6cc36d9e758a6484dce7432f568cbd
emiliaplater <herman-dev@outlook.com>
2023-04-27 09:55:49 +0200

    BM: trackbar and filters added

--- Modules/binaryModule.py
+++ Modules/binaryModule.py
@@ -1,9 +1,27 @@
 import cv2
+import numpy as np
+
+
+
+def on_trackbar(val):
+    global threshold_value
+    threshold_value = val
+
+cv2.namedWindow('Processed Frame')
+cv2.createTrackbar('Threshold', 'Processed Frame', 20, 255, on_trackbar)
+threshold_value = 20
 
 
 def binaryModule(frame): 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-    _, frame_threshold = cv2.threshold(gray_frame, 20, 255, cv2.THRESH_BINARY_INV)
+
+    _, threshold = cv2.threshold(gray_frame, threshold_value, 255, cv2.THRESH_BINARY_INV)
+
+    kernel = np.ones((3, 3), np.uint8)
+    opened = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
+    frame_threshold = cv2.medianBlur(closed, 7)
+
 
     return frame_threshold 
