2667ec4aa68fd8558a63515c078f75347e0145f0
emiliaplater <herman-dev@outlook.com>
2023-04-27 10:18:24 +0200

    BM: low-light problem fixed

--- Modules/binaryModule.py
+++ Modules/binaryModule.py
@@ -8,20 +8,22 @@ def on_trackbar(val):
     threshold_value = val
 
 cv2.namedWindow('Processed Frame')
-cv2.createTrackbar('Threshold', 'Processed Frame', 20, 255, on_trackbar)
-threshold_value = 20
+cv2.createTrackbar('Threshold', 'Processed Frame', 13, 255, on_trackbar)
+threshold_value = 13
 
 
 def binaryModule(frame): 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
-    _, threshold = cv2.threshold(gray_frame, threshold_value, 255, cv2.THRESH_BINARY_INV)
+    brightness = np.mean(gray_frame)
+    threshold = threshold_value + int((brightness - 128) / 2)
+
+    _, threshold = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY_INV)
 
     kernel = np.ones((3, 3), np.uint8)
     opened = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
     closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
     frame_threshold = cv2.medianBlur(closed, 7)
-
 
     return frame_threshold 
