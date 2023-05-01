25418961380a015535b9f0892c637f5827d2db1d
emiliaplater <herman-dev@outlook.com>
2023-05-01 21:16:53 +0200

    MM: rebuild -v0.4

--- Modules/mainModule.py
+++ Modules/mainModule.py
@@ -1,7 +1,7 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
-#modules
+import time
+# Modules
 from binaryModule import binaryModule
 from algorithmModule1 import algorithmModule1
 from algorithmModule2 import algorithmModule2
@@ -13,6 +13,13 @@ class MainModule:
     def __init__(self, flv_path, time_frame):
         self.flv_path = flv_path
         self.time_frame = time_frame
+        
+        fig = plt.figure()
+        self.ax = fig.add_subplot(111, projection='3d')
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.set_zlabel('Time')
+
 
     def play(self):
         capture = cv2.VideoCapture(self.flv_path)
@@ -24,6 +31,7 @@ class MainModule:
 
             threshold_frame = binaryModule(frame)
 
+
             left_eye_coords1, right_eye_coords1 = algorithmModule1(threshold_frame, frame)
             left_eye_coords2, right_eye_coords2 = algorithmModule2(threshold_frame, frame)
             
@@ -34,22 +42,34 @@ class MainModule:
                 right_eye_coords2
             )
 
+            timestamp = time.time()
+
+            if left_eye_average_X is not None:
+                self.ax.scatter(left_eye_average_X, left_eye_average_Y, timestamp, marker='o', color='blue')
+            if right_eye_average_X is not None:
+                self.ax.scatter(right_eye_average_X, right_eye_average_Y, timestamp, marker='o', color='purple')
+
+            plt.pause(0.001)
+
+
             height, width = frame.shape[:2]
             half_width = width // 2
             left_frame = frame[:, :half_width]
             right_frame = frame[:, half_width:]
 
-
             cv2.imshow('Both', frame)
             cv2.imshow('Left', left_frame)
             cv2.imshow('Right', right_frame)
+
 
             key = cv2.waitKey(self.time_frame) & 0xFF
             if key == ord('q'):
                 break
 
+
         capture.release()
         cv2.destroyAllWindows()
 
-video_player = MainModule('../vids/vid1.flv', 77) # prodive a path
+
+video_player = MainModule('#', 1) # prodive a path
 video_player.play()
