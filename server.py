7364f312f9a2107137763b91d03fbc8ee5db872f
emiliaplater <herman-dev@outlook.com>
2023-05-26 19:03:25 +0200

    server and exceptions added

--- server.py
+++ server.py
@@ -3,50 +3,75 @@ import zipfile
 import tornado.ioloop
 import tornado.web
 from mainModule import MainModule
+from utils.exceptions.exceptions import InvalidVideoFile, NoVideoFileSelected
 
 class ProcessVideoHandler(tornado.web.RequestHandler):
     def post(self):
-        video_file = self.request.files['video'][0]
+        try:
+            video_files = self.request.files.get('video')
+            if not video_files:
+                raise NoVideoFileSelected('No video file selected.')
 
-        input_videos_folder = './input_videos'
-        os.makedirs(input_videos_folder, exist_ok=True)
+            video_file = video_files[0]
+            video_filename = video_file['filename']
 
-        video_path = os.path.join(input_videos_folder, video_file['filename'])
-        with open(video_path, 'wb') as f:
-            f.write(video_file['body'])
+            # Check if the file extension is mp4
+            if not video_filename.endswith('.mp4'):
+                raise InvalidVideoFile('Invalid video file. Only .mp4 files are allowed.')
 
-        video_player = MainModule(video_path, 1, output_folder='./output_videos')
-        video_player.play()
+            input_videos_folder = './input_videos'
+            os.makedirs(input_videos_folder, exist_ok=True)
 
-        processed_video_folder = video_player.output_folder
+            video_path = os.path.join(input_videos_folder, video_filename)
+            with open(video_path, 'wb') as f:
+                f.write(video_file['body'])
 
-        zip_path = os.path.join(processed_video_folder, f'details_for_{video_player.video_name}.zip')
-        with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, dirs, files in os.walk(processed_video_folder):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    arcname = os.path.relpath(file_path, processed_video_folder)
-                    zipf.write(file_path, arcname)
+            video_player = MainModule(video_path, 1, output_folder='./output_videos')
+            video_player.play()
 
-        self.set_header('Content-Type', 'application/octet-stream')
-        self.set_header('Content-Disposition', 'attachment; filename=output_folder.zip')
+            processed_video_folder = video_player.output_folder
 
-        with open(zip_path, 'rb') as f:
-            while True:
-                chunk = f.read(4096)
-                if not chunk:
-                    break
-                self.write(chunk)
-                self.flush()
+            zip_path = os.path.join(processed_video_folder, f'details_for_{video_player.video_name}.zip')
+            with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for root, dirs, files in os.walk(processed_video_folder):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        arcname = os.path.relpath(file_path, processed_video_folder)
+                        zipf.write(file_path, arcname)
 
-        self.finish()
+            self.set_header('Content-Type', 'application/octet-stream')
+            self.set_header('Content-Disposition', 'attachment; filename=output_folder.zip')
 
-        os.remove(zip_path)
+            with open(zip_path, 'rb') as f:
+                while True:
+                    chunk = f.read(4096)
+                    if not chunk:
+                        break
+                    self.write(chunk)
+                    self.flush()
+
+            self.finish()
+
+            os.remove(zip_path)
+
+        except NoVideoFileSelected as e:
+            self.set_status(e.status_code)
+            self.write({'error': e.message})
+
+        except InvalidVideoFile as e:
+            self.set_status(e.status_code)
+            self.write({'error': e.message})
+
+        except Exception as e:
+            self.set_status(500)
+            self.write({'error': 'An unexpected error occurred.'})
+
 
 def make_app():
     return tornado.web.Application([
         (r"/process_video", ProcessVideoHandler),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
